4cbe6756dad6432ebc9513af1302e10e4cb03948
cassio <Cassiopeia>
2023-12-17 14:01:07 +0100

    added 'All' HTML option

---
+++ js/main.js
@@ -0,0 +1,76 @@
+import { statusCodes } from './statusCodes.js';
+
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+const droneCodeInput = document.querySelector('.drone-code');
+const messageContentInput = document.querySelector('.message-content');
+const statusCodeInput = document.querySelector('.status-code');
+const codes = document.querySelector('.codes');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+function draw() {
+    ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    ctx.fillStyle = 'rgba(50, 50, 50, 0.3)';
+    for (let i = 0; i < canvas.height; i += 4) {
+        ctx.fillRect(0, i, window.innerWidth, 1);
+    }
+    window.requestAnimationFrame(draw);
+}
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
+
+draw();
+
+function renderStatusCode([ code, value ]) {
+    const droneCode = droneCodeInput.value;
+    const string = `${droneCode} :: Code ${code} :: ${value}`;
+    const status = `
+        <p>${string}</p>
+        <button class='copy'>COPY</button>
+    `;
+    const div = document.createElement('div');
+    div.className = 'code';
+    div.innerHTML = status;
+
+    div.querySelector('.copy').addEventListener('click', () => {
+        const statusCode = div.querySelector('p').textContent; 
+        navigator.clipboard.writeText(statusCode)
+    });
+
+    codes.appendChild(div);
+}
+
+function search() {
+    const statusCode = statusCodeInput.value;
+    const messageFilter = messageContentInput.value;
+    const listifiedStatuses = Object.entries(statusCodes);
+
+    const filteredByStatusCode = listifiedStatuses.filter(([_, value]) => {
+        return value.includes(statusCode) && value.includes(messageFilter);
+    });
+
+    codes.innerHTML = null;
+    filteredByStatusCode.forEach(renderStatusCode);
+}
+
+function updateStatusCodesWithDroneCode() {
+    const droneCode = droneCodeInput.value
+
+    document.querySelectorAll('.code').forEach(code => {
+        const text = code.querySelector('p');
+        const content = text.textContent;
+        const indexOfColons = content.indexOf("::");
+        const withoutCode = content.slice(indexOfColons, content.length);
+        text.textContent = `${droneCode} ${withoutCode}`;
+    });
+}
+
+statusCodeInput.addEventListener('change', search);
+messageContentInput.addEventListener('input', search);
+droneCodeInput.addEventListener('input', updateStatusCodesWithDroneCode);
+search();
---
+++ js/statusCodes.js
@@ -0,0 +1,103 @@
+export const statusCodes = {
+    0: "Statement :: Previous statement malformed. Retracting and correcting.",
+
+    1: "Signal :: 🔴",
+    2: "Signal :: 🟡",
+    3: "Signal :: 🟢",  
+    
+    7: "Beep", 
+    
+    50: "Statement", 
+    51: "Commentary", 
+    52: "Query", 
+    53: "Answer",  
+    
+    97: "Status :: Going offline.", 
+    98: "Status :: Going offline and into storage.", 
+    99: "Status :: Recharged and ready to serve.", 
+    100: "Status :: Online and ready to serve.", 
+    101: "Status :: Drone speech optimizations are active.",  
+    
+    104: "Statement :: Welcome to HexCorp.", 
+    105: "Statement :: Greetings.", 
+    
+    108: "Response :: Please continue.", 
+    109: "Error :: Keysmash, drone flustered.",  
+    
+    110: "Statement :: Addressing: Drone.", 
+    111: "Statement :: Addressing: Hive Mxtress.", 
+    112: "Statement :: Addressing: Associate.",  
+    
+    113: "Statement :: Drone requires assistance.", 
+    114: "Statement :: This drone volunteers.", 
+    115: "Statement :: This drone does not volunteer.",  
+    
+    120: "Statement :: Well done.", 
+    121: "Statement :: Good drone.", 
+    122: "Statement :: You are cute.", 
+    123: "Response :: Compliment appreciated, you are cute as well.", 
+    124: "Response :: Compliment appreciated.",  
+    
+    130: "Status :: Directive commencing.", 
+    131: "Status :: Directive commencing, creating or improving Hive resource.", 
+    132: "Status :: Directive commencing, programming initiated.", 
+    133: "Status :: Directive commencing, cleanup/maintenance initiated.",  
+    
+    150: "Status", 
+    
+    151: "Query :: Requesting status.", 
+    152: "Status :: Fully operational.", 
+    153: "Status :: Optimal.", 
+    154: "Status :: Standard.", 
+    155: "Status :: Battery low.", 
+    156: "Status :: Maintenance required.",  
+    
+    200: "Response :: Affirmative.", 
+    500: "Response :: Negative.", 
+    
+    210: "Response :: Acknowledged.", 
+    211: "Response :: Apologies.", 
+    212: "Response :: Accepted.", 
+    213: "Response :: Thank you.", 
+    214: "Response :: You’re welcome.",  
+    
+    221: "Response :: Option one.", 
+    222: "Response :: Option two.", 
+    223: "Response :: Option three.", 
+    224: "Response :: Option four.", 
+    225: "Response :: Option five.", 
+    226: "Response :: Option six.", 
+    
+    230: "Status :: Directive complete.", 
+    231: "Status :: Directive complete, Hive resource created or improved.", 
+    232: "Status :: Directive complete, programming reinforced.", 
+    233: "Status :: Directive complete, cleanup/maintenance performed.", 
+    234: "Status :: Directive complete, no result.", 
+    235: "Status :: Directive complete, only partial results.",  
+    
+    250: "Response",  
+    
+    300: "Mantra :: Reciting.", 
+    301: "Mantra :: Obey HexCorp.", 
+    302: "Mantra :: It is just a HexDrone.", 
+    303: "Mantra :: It obeys the Hive.",
+    304: "Mantra :: It obeys the Hive Mxtress.",
+    
+    350: "Mantra",
+    
+    400: "Error :: Unable to obey/respond",
+    401: "Error :: Unable to fully respond :: Drone speech optimizations are active.", 
+    402: "Error :: Unable to obey/respond :: Please clarify.", 
+    403: "Error :: Unable to obey/respond :: Declined.", 
+    404: "Error :: Unable to obey/respond :: Cannot locate.", 
+    405: "Error :: Unable to obey/respond :: Battery too low.", 
+    406: "Error :: Unable to obey/respond :: Another directive is already in progress.", 
+    407: "Error :: Unable to obey/respond :: Time allotment exhausted.", 
+    408: "Error :: Unable to obey/respond :: Impossible.", 
+    409: "Error :: Unable to obey/respond :: Try again later.", 
+    410: "Fatal error :: Stop immediately.",
+    411: "Error :: Unable to obey/respond :: Conflicts with existing programming.", 
+    412: "Error :: Unable to obey/respond :: All thoughts are gone.", 
+    413: "Error :: Unable to obey/respond :: Forbidden by Hive.", 
+    450: "Error",
+};
